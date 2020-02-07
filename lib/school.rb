2777ff84c1a5dc2fab0f010077586f0ed1d7d58c
james Johnson <2013matthew2013@gmail.com>
2020-02-07 02:28:02 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name,grade)
+    #roster is a hash
+    
+    if @roster[grade]
+    roster[grade] << name 
+  else 
+    @roster[grade] = [name]
+  end
+end  
+  
+  def grade(grade)
+    #roster 9 => ["samename"]
+    roster[grade]
+  end
+  
+  def sort 
+    @roster.each do |key, value|
+      @roster[key] = value.sort
+    end
+  end 
+end
+    
